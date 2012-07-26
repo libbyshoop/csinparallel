23b5943a26da984b3e102f3f6a7e5da9a0c0682b
yzhao1 <yzhao1@macalester.edu>
2012-07-26 17:42:41 -0500

    updating conf.py file of Heterogeneous module

--- modules/HeterogeneousProgramming/source/conf.py
+++ modules/HeterogeneousProgramming/source/conf.py
@@ -25,7 +25,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.pngmath', 'sphinx.ext.todo', ]
 
 if 'Darwin' in os.uname()[0]:
 	pngmath_latex = '/usr/local/texlive/2011/bin/x86_64-darwin/latex'
