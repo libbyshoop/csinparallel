a88b019c946b38ed723f651a4e0a9ced0a665a33
Stephen N. Lee <leesn@stolaf.edu>
2013-07-02 17:10:31 -0500

    Built the first html documents

---
+++ modules/RiverTrail/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({objects:{},terms:{rivertrail:0,content:0,mapreduc:0,welcom:0,document:0},objtypes:{},titles:["Welcome to MapReduce with RiverTrail&#8217;s documentation!"],objnames:{},filenames:["index"]})
