de69826899b341575048dcae70b07e0d80054615
Stephen N. Lee <leesn@stolaf.edu>
2013-07-31 17:31:00 -0500

    Modified the Phi page to not have 's documentation

--- dev/modules/IntelPhi/source/conf.py
+++ dev/modules/IntelPhi/source/conf.py
@@ -183,7 +183,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'ProgrammingwiththeIntelPhiCoprocessor.tex', u'Programming with the Intel Phi Coprocessor Documentation',
+  ('index', 'ProgrammingwiththeIntelPhiCoprocessor.tex', u'Programming with the Intel Phi Coprocessor',
    u'CSInParallel Project', 'manual'),
 ]
 
--- dev/modules/IntelPhi/source/index.rst
+++ dev/modules/IntelPhi/source/index.rst
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Programming with the Intel Phi Coprocessor's documentation!
+Welcome to Programming with the Intel Phi Coprocessor
 ======================================================================
 
 Contents:
