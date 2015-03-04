3e1314c5ba5434211e84a85d57090d1fc693ef72
Libby Shoop <shoop@macalester.edu>
2015-03-04 14:52:38 -0600

    Ptternlets update for MPI restructuring and additions

--- modules/Patternlets/source/patternlets/OpenMP/08.reduction/reduction.c
+++ modules/Patternlets/source/patternlets/OpenMP/08.reduction/reduction.c
@@ -28,7 +28,7 @@ int main(int argc, char** argv) {
    int array[SIZE];
 
    if (argc > 1) {
-        mp_set_num_threads( atoi(argv[1]) );
+        omp_set_num_threads( atoi(argv[1]) );
    }
 
    initialize(array, SIZE);
